ee36d47ed3273793ef423cb57f9e275bbf838506
xiaoxi.li <lixiaoxi5@163.com>
2018-09-10 07:42:26 +0800

    初始提交，少很多东西

---
+++ XPZS/__init__.py
@@ -0,0 +1,4 @@
+import pymysql
+
+# 初始化Mysqldb
+pymysql.install_as_MySQLdb()
---
+++ cmdb/views.py
@@ -0,0 +1,40 @@
+# Create your views here.
+# 用于跳转页面
+from django.core import serializers
+from django.shortcuts import render
+# 返回值载体
+from django.shortcuts import HttpResponse
+# Model对象
+# TODO MVP分层存在问题
+from cmdb import models
+# JSON支持
+import json
+
+
+# 打印HelloWord
+def helloWord(request):
+	return HttpResponse("Hello Word!")
+
+
+# 跳转index页面
+def index(request):
+	return render(request, 'index.html')
+
+
+# 提交表单
+def submit(request):
+	if request.method == 'POST':
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+		print(username, password)
+	return HttpResponse('username=' + username + ", password=" + password)
+
+
+# 搜索数据
+def query(request):
+	orderCode = request.GET.get("id", None)
+	list = []
+
+	if orderCode != None:
+		list = models.MyTask.objects.filter(order_code=orderCode)
+	return HttpResponse(serializers.serialize('json', list), 'application/json')
